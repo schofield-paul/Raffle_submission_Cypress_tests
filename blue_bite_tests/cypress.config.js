a90f0947dc8ee48e12a6e5beeb1bc6383b322879
Paul Schofield <paul.schofield.us@gmail.com>
2023-06-25 10:45:56 -0400

    Initial commit

---
+++ blue_bite_tests/cypress.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl:
+      'https://blue-bite-dev-3.bluebite.io/04425f7c-4fdd-47f6-85b3-b800d12bb9ca',
+  },
+});
---
+++ blue_bite_tests/cypress/e2e/spec.cy.js
@@ -0,0 +1,29 @@
+const testData = require('../../data/testData.js');
+
+describe('Validate positive user flow once', () => {
+  testData.forEach((data, index) => {
+    it(`Test ${index + 1}: clicks the link "type"`, () => {
+      cy.visit('/');
+
+      // Get name input, type into it
+      cy.get('#input-3').type(data.name);
+
+      // Get email input, type into it
+      cy.get('#input-4').type(data.email);
+
+      // Get email input, type into it
+      cy.get('#input-8').type(data.age);
+
+      // Submit form
+      cy.get('button.button-sc-1ta45yz-0').click();
+
+      cy.get('div.snippet__Body-sc-12bo3rm-0.cmpVnN strong')
+        .invoke('text')
+        .should('eq', String(index + 1));
+
+      if (index !== testData.length - 1) {
+        cy.reload();
+      }
+    });
+  });
+});
