dd9c01d792eb24506355263ab17df9cddeafd85b
Paul Schofield <paul.schofield.us@gmail.com>
2023-06-25 16:21:32 -0400

    validation failure tests

---
+++ blue_bite_tests/cypress/e2e/input_validations.cy.js
@@ -0,0 +1,136 @@
+const {
+  testData1,
+  testData2,
+  testData3,
+  testData4,
+} = require('../../data/input_validations_data.js');
+
+Cypress.session.clearCurrentSessionData();
+
+//Pairwise testing
+
+// describe('Validate errors with bad age inputs', () => {
+//   testData1.forEach((data, index) => {
+//     it(`Test ${index + 1}: Submit valid input; return count 1`, () => {
+//       cy.visit('/');
+
+//       // Get name input, type into it
+//       cy.get('#input-3').type(data.name);
+
+//       // Get email input, type into it
+//       cy.get('#input-4').type(data.email);
+
+//       // Get email input, type into it
+//       if (index !== testData1.length - 1) {
+//         // Not the last iteration, type the age input
+//         cy.get('#input-8').type(data.age);
+//       }
+
+//       // Submit form
+//       cy.get('button.button-sc-1ta45yz-0').click();
+
+//       cy.contains(
+//         'div.snippet__Body-sc-12bo3rm-0.cmpVnN',
+//         'Must be 18 or older to enter.'
+//       ).should('be.visible');
+
+//       if (index !== testData1.length - 1) {
+//         cy.reload();
+//       }
+//     });
+//   });
+// });
+
+// describe('Validate errors with bad email inputs', () => {
+//   testData2.forEach((data, index) => {
+//     it(`Test ${index + 1}: Submit valid input; return count 1`, () => {
+//       cy.visit('/');
+
+//       // Get name input, type into it
+//       cy.get('#input-3').type(data.name);
+
+//       // Get email input, type into it
+//       cy.get('#input-4').type(data.email);
+
+//       // Get email input, type into it
+//       // Not the last iteration, type the age input
+//       cy.get('#input-8').type(data.age);
+
+//       // Submit form
+//       cy.get('button.button-sc-1ta45yz-0').click();
+
+//       cy.contains('div.snippet__Body-sc-12bo3rm-0.cmpVnN', 'email').should(
+//         'be.visible'
+//       );
+
+//       if (index !== testData2.length - 1) {
+//         cy.reload();
+//       }
+//     });
+//   });
+// });
+
+// describe('Validate errors with bad name inputs', () => {
+//   testData3.forEach((data, index) => {
+//     it(`Test ${index + 1}: Submit valid input; return count 1`, () => {
+//       cy.visit('/');
+
+//       // Get name input, type into it if not the last iteration
+//       if (index !== testData3.length - 1) {
+//         cy.get('#input-3').type(data.name);
+//       }
+
+//       // Get email input, type into it
+//       cy.get('#input-4').type(data.email);
+
+//       // Get email input, type into it
+//       // Not the last iteration, type the age input
+//       cy.get('#input-8').type(data.age);
+
+//       // Submit form
+//       cy.get('button.button-sc-1ta45yz-0').click();
+
+//       cy.contains(
+//         'div.snippet__Body-sc-12bo3rm-0.cmpVnN',
+//         'name invalid'
+//       ).should('be.visible');
+
+//       if (index !== testData3.length - 1) {
+//         cy.reload();
+//       }
+//     });
+//   });
+// });
+
+// describe('Validate errors with combinations of bad input', () => {
+//   testData4.forEach((data, index) => {
+//     it(`Test ${index + 1}: Submit valid input; return count 1`, () => {
+//       cy.visit('/');
+
+//       // toggle fields as needed
+//       cy.get('#input-3').type(data.name);
+
+//       // Get email input, type into it
+//       //cy.get('#input-4').type(data.email);
+
+//       // Get email input, type into it
+//       // Not the last iteration, type the age input
+//       // cy.get('#input-8').type(data.age);
+
+//       // Get Reason input, type into it
+//       // cy.get('#input-9').type(data.reason);
+
+//       // Submit form
+//       cy.get('button.button-sc-1ta45yz-0').click();
+
+//       cy.contains(
+//         'div.snippet__Body-sc-12bo3rm-0.cmpVnN',
+//         'name invalid'
+//       ).should('be.visible');
+
+//       if (index !== testData4.length - 1) {
+//         cy.reload();
+//       }
+//     });
+//   });
+// });
---
+++ blue_bite_tests/data/input_validations_data.js
@@ -0,0 +1,108 @@
+const testData1 = [
+  {
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: ' ',
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: 'string',
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: -18,
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: 0,
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: 16,
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: 17,
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: null,
+    reason: 'I am feeling Lucky!',
+  },
+];
+
+const testData2 = [
+  {
+    name: 'Alice',
+    email: 'aliceexamplecom',
+    age: 20,
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 'Alice',
+    email: 'aliceexamplecom',
+    age: 20,
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 'Alice',
+    email: 'aliceexamplecom',
+    age: 20,
+    reason: 'I am feeling Lucky!',
+  },
+];
+
+const testData3 = [
+  {
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: 20,
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 7,
+    email: 'alice@example.com',
+    age: 20,
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: 'five@gmail.com',
+    email: 'alice@example.com',
+    age: 20,
+    reason: 'I am feeling Lucky!',
+  },
+  {
+    name: '  ',
+    email: 'alice@example.com',
+    age: 20,
+    reason: 'I am feeling Lucky!',
+  },
+];
+
+const testData4 = [
+  {
+    name: 'Alice',
+    email: 'alice@example.com',
+    age: 20,
+    reason: 'I am feeling Lucky!',
+  },
+];
+
+module.exports = {
+  testData1,
+  testData2,
+  testData3,
+  testData4,
+};
